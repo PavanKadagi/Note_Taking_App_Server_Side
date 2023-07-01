f1c3a53b8622a60039040a48552b0f892525c215
abc <abc@example.com>
2023-07-01 10:59:34 +0530

    changes

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cors({
-    origin:["http://localhost:3000","https://note-taking-app-server-su0d.onrender.com"]
+    origin:["http://localhost:3000","https://note-taking-app-server-su0d.onrender.com"],
+    optionsSuccessStatus: 200,
+   credentials:true,
+   methods:["GET","PUT","PATCH","POST","DELETE","OPTIONS"],
+   allowedHeaders:[
+    'Access-Control-Allow-Origin',
+    'Content-Type',
+    'Authorization',
+   ],
 }))
 app.use(require('./routes/user'))
 
